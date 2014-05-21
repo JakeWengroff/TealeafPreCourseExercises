208c041e963ab62c9a9811b1f61d126c4c076888
Jake Wengroff <j@jxb1.com>
2014-05-20 22:35:57 -0400

    Create exercise_16.rb

---
+++ exercise_16.rb
@@ -0,0 +1,10 @@
+# exercise_16.rb
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+a = a.map { |pairs| pairs.split }
+
+a = a.flatten
+
+p a
